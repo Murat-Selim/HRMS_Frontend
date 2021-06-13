7d8f0795eee62a361351b13d6bfcd2e84f527644
Murat-Selim <66679051+Murat-Selim@users.noreply.github.com>
2021-06-13 18:05:34 +0300

    update

--- src/layouts/Navbar/Navi.jsx
+++ src/layouts/Navbar/Navi.jsx
@@ -1,33 +1,38 @@
-import React, { useState } from 'react';
-import { NavLink, useHistory } from 'react-router-dom';
-import { Container, Menu} from 'semantic-ui-react';
-import SignedIn from './SignedIn'
-import SignedOut from './SignedOut'
+import React, { useState } from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import {Container, Menu} from "semantic-ui-react";
+import SignedIn from "./SignedIn";
+import SignedOut from "./SignedOut";
 
 export default function Navi() {
   const [isAuthenticated, setIsAuthenticated] = useState(true);
   const history = useHistory();
 
-  function handleSignOut(){
-      setIsAuthenticated(false)
-      history.push("/")
+  function handleSignOut() {
+    setIsAuthenticated(false);
+    history.push("/");
   }
 
-  function handleSignIn(){
-    setIsAuthenticated(true)
+  function handleSignIn() {
+    setIsAuthenticated(true);
   }
+
   return (
     <div>
       <Menu inverted fixed="top">
         <Container>
-          <Menu.Item name="HRMS"/> 
-           
+          <Menu.Item name="HRMS" />
+          
           <Menu.Item as={NavLink} to="/jobPositionList" name="Ana Sayfa" icon="home"/>
-          <Menu.Item as={NavLink} to="/jobAdvertList" name="Is ilanlari"/>
-          <Menu.Item name="Cv'ler"/>
+          <Menu.Item as={NavLink} to="/jobAdvertList" name="İş ilanlari" />
+          <Menu.Item name="Cvler" />
 
           <Menu.Menu position="right">
-            {isAuthenticated? <SignedIn signOut={handleSignOut}/>:<SignedOut signIn={handleSignIn}/>}
+            {isAuthenticated ? (
+              <SignedIn signOut={handleSignOut} />
+            ) : (
+              <SignedOut signIn={handleSignIn} />
+            )}
           </Menu.Menu>
         </Container>
       </Menu>
